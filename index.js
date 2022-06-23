25830cbe1fce9a051de737bae8e861073fab8a7f
Thanet-Bus <76712551+Thanet-Bus@users.noreply.github.com>
2022-06-23 10:51:12 +0700

    Add cors list

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ const config = {
 let TOKEN =
   "VGiLhUm7XgQc1PTql/7G3Hmtb6dcuH4qsIjujHuXRuNzNfKYcvY9Y/LUVtSDLedp7uX9ItH8FvGwRmqwjRLKPU1ts3z1b1oE6qRrLDh/zJQvVGAdHvS6nbVgmfY2NwA1r+nKspZRomAMbhDaY15h6QdB04t89/1O/w1cDnyilFU=";
 
-app.use("/reject-order"||"/finished-order",express.json());
+app.use("/reject-order" || "/finished-order", express.json());
 
 // differnt port request each other
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://woxa-food-order.herokuapp.com"],
   credentials: true,
 };
 app.use(cors(corsOptions));
@@ -115,7 +115,7 @@ app.post("/finished-order", function (req, res) {
   res.send("sent message");
 });
 
-app.use("/webhook",middleware(config));
+app.use("/webhook", middleware(config));
 
 // Waiting user response to send reply message
 app.post("/webhook", function (req, res) {
